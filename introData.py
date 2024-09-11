b44e9a47b4c6344a948740d059bdfb8af2117d58
pacot2 <pacot2@nycstudents.net>
2024-09-11 09:18:23 -0400

    almost done with madlibs thing

---
+++ introData.py
@@ -0,0 +1,19 @@
+""" first going to set a variable and ask for input a sentence """
+""" after, going to establish a variable that determines the word count in the sentence"""
+sentence = input("Input a sentence for this test")
+wordCount = 0
+
+def outputSentence():
+    print(f"You wrote, '{sentence}'.")
+    print("...")
+    print("...")
+    print("...")
+    wordCount = len(sentence.split())
+    if wordCount == 1:
+        print(f"There is 1 word in your sentence.")
+        print("...")
+        print("...")
+        print("Write more, you wimp!")
+    else:
+        print(f"There are {wordCount} words in your sentence.")
+outputSentence()
---
+++ madlibs.py
@@ -0,0 +1,11 @@
+verb1 = input("Choose one verb.")
+verb2 = input("Choose another verb.")
+if verb1 == verb2:
+    print("Please do not choose the same verb twice!")
+else:
+    noun = input("Please choose a noun.")
+    number = input("Please choose a number.")
+    celeb = input("Input a celebrity of your choice.")
+    adj = input("Lastly, choose an adjective.")
+    madlib = f"{adj} {verb1}  {verb2}  {noun}  {number}  {celeb}"
+    print(f"It's a {adj} day outside. As you {verb1} in preparation to {verb2} later in the day, {number} of {noun} stops you! As you start to panic, you look around to see {celeb} watching you from across the street, getting ready to save you. You breathe a sigh of relief, but you come to realize {celeb} is not rooting for you. ")
