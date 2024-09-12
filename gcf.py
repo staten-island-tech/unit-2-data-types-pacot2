883f865531e7607fc9cb0ef2bdb53a192a5e8871
pacot2 <pacot2@nycstudents.net>
2024-09-12 09:22:12 -0400

    completed minor things

---
+++ gcf.py
@@ -0,0 +1,6 @@
+num1 = int(input("Input one number... "))
+num2 = int(input("Input a second number... "))
+print = ("We will now find the greatest common factor of both of these numbers.")
+if num1 != num2:
+    math.gcd(num1,num2)
+    
