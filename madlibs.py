b623ca8693d6bd357bcba0d7438b580cfb908a08
pacot2 <pacot2@nycstudents.net>
2024-09-12 08:41:03 -0400

    minor adjustments

--- madlibs.py
+++ madlibs.py
@@ -8,4 +8,4 @@ else:
     celeb = input("Input a celebrity of your choice.")
     adj = input("Lastly, choose an adjective.")
     madlib = f"{adj} {verb1}  {verb2}  {noun}  {number}  {celeb}"
-    print(f"It's a {adj} day outside. As you {verb1} in preparation to {verb2} later in the day, {number} of {noun} stops you! As you start to panic, you look around to see {celeb} watching you from across the street, getting ready to save you. You breathe a sigh of relief, but you come to realize {celeb} is not rooting for you. ")
+    print(f"It's a {adj} day outside. As you {verb1} in preparation to {verb2} later in the day, {number} of {noun} stops you! As you start to panic, you look around to see {celeb} watching you from across the street, getting ready to save you. You breathe a sigh of relief, but you come to realize {celeb} does not seem to happy with you.")
