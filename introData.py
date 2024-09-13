1ba114b9083b05b7d687dbbd9ba6196e950ee993
pacot2 <pacot2@nycstudents.net>
2024-09-13 09:18:07 -0400

    probably done idk

--- introData.py
+++ introData.py
@@ -1,5 +1,3 @@
-""" first going to set a variable and ask for input a sentence """
-""" after, going to establish a variable that determines the word count in the sentence"""
 sentence = input("Input a sentence... ")
 wordCount = 0
 
@@ -11,9 +9,6 @@ def outputSentence():
     wordCount = len(sentence.split())
     if wordCount == 1:
         print(f"There is 1 word in your sentence.")
-        print("...")
-        print("...")
-        print("Write more, you wimp!")
     else:
         print(f"There are {wordCount} words in your sentence.")
 outputSentence()
