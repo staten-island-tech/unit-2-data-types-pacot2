138588627e0f6d8293ab04b274f296c270463196
pacot2 <pacot2@nycstudents.net>
2024-09-17 08:46:40 -0400

    uagghhhhhhhhhhhhhhhhhhhhhhhh

--- findFactors.py
+++ findFactors.py
@@ -1,4 +1,4 @@
-def factors(x):
+def callFactors(x):
    print("The factors of",x,"are:")
    for i in range(1, x + 1):
        if x % i == 0:
@@ -6,4 +6,4 @@ def factors(x):
 
 number = int(input("Input a number and all of its factors will appear! "))
 
-factors(number)
+callFactors(number)
