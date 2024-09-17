ad19dd60703f94efc55721f9d386527963744f40
pacot2 <pacot2@nycstudents.net>
2024-09-17 09:08:54 -0400

    updated findfactors
    updated to store factors inside a list called factorStorage instead of just printing them like a loser

--- findFactors.py
+++ findFactors.py
@@ -1,9 +1,11 @@
+factorStorage = []
 def callFactors(x):
    print("The factors of",x,"are:")
    for i in range(1, x + 1):
        if x % i == 0:
-           print(i)
+           factorStorage.append(i)
 
 number = int(input("Input a number and all of its factors will appear! "))
 
 callFactors(number)
+print(factorStorage)
