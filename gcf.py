4c5ba7b83ca8d756b87af8a602cb4682a4e771b4
pacot2 <pacot2@nycstudents.net>
2024-09-18 09:13:57 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -3,7 +3,7 @@ num2 = int(input("Input a second number... "))
 gcf = 1
 
 if num1 == num2:
-    print(f"The greatest common factor of {num1} and {num2} is {num1}.")
+    print(f"The greatest common factor of {num1} and {num2} is {num1}. They're the same number!")
 else:
     for i in range(1, max(num1, num2)):
         if num1 % i == 0 and num2 % i == 0:
@@ -11,5 +11,6 @@ else:
     if gcf == 1:
         print(f"The greatest common factor between {num1} and {num2} is 1!")
         print("This means that your two numbers essentially do not share any common factors.")
+        print("... other than 1, of course.")
     else:
         print(f"The greatest common factor of {num1} and {num2} is {gcf}")
