adb76298267a41ff22cab9eb82cfdbd8d917a515
pacot2 <pacot2@nycstudents.net>
2024-09-19 09:20:16 -0400

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,29 +1,27 @@
 bill = 0
 running = 0
+owedAmount = 0
 while running <= 49:
     quality = input("Thanks for dining with us today! How was our service today? Was it bad, okay, good, or great? ")
     if quality == "bad" or quality == "Bad":
         print("We're so sorry. ")
-        bill = 0
-        print(f"We suggest tipping your server {bill}% of your bill.")
-        break
+        bill = 1
+        print(f"We suggest tipping your server 0% of your bill.")
     elif quality == "okay" or quality == "Okay" or quality == "ok" or quality == "Ok" or quality == "OK":
         print("We're sorry if the service did not meet your expectations. ")
-        bill = 15
-        print(f"We suggest tipping your server {bill}% of your bill.")
-        break
+        bill = 1.15
+        print(f"We suggest tipping your server 15% of your bill.")
     elif quality == "good" or quality == "Good":
         print("Thank you for the rating!")
-        bill = 20
-        print(f"We suggest tipping your server {bill}% of your bill.")
-        break
+        bill = 1.20
+        print(f"We suggest tipping your server 20% of your bill.")
     elif quality == "great" or quality == "Great":
         print("Thank you for the exceptional rating!")
-        bill = 25
-        print(f"We suggest tipping your server {bill}% of your bill.")
-        break
+        bill = 1.25
+        print(f"We suggest tipping your server 25% of your bill.")
     else:
         print("I'm sorry, something went wrong. Try again. ")
-        running += 1
-if running >= 50:
-    print("Why are you inputting so much? Please stop... Please learn some patience.")
+        running = 100
+    owedAmount = int(input("What's your bill amount? Don't use the dollar sign. "))
+    newAmount = owedAmount * bill
+    print(f"Your suggested bill is {newAmount}.")
